17909a82289a605fe22dc3631ae430a18a74d498
Bernat Brunet <bernat@nan-tic.com>
2020-12-07 14:39:37 +0100

    Add missing Workflow import

--- production.py
+++ production.py
@@ -1,9 +1,9 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
+from trytond.model import ModelView, Workflow
 from trytond.pool import PoolMeta, Pool
 from trytond.i18n import gettext
 from trytond.exceptions import UserWarning
-from trytond.model import ModelView
 
 __all__ = ['Production']
 
